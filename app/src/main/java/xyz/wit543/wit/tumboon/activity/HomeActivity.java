c48782025130a757f5444166201db7afad333732
Norawit Urailertprasert <wit543@gmail.com>
2016-05-21 20:55:47 +0700

    permission access for travis

---
+++ app/src/main/java/xyz/wit543/wit/tumboon/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package xyz.wit543.wit.tumboon.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xyz.wit543.wit.tumboon.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
